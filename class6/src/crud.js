fd4c60c98491020c87993a49aed8179aa2a998e7
zahid-mughal <zahisohna@gmail.com>
2021-07-31 11:11:21 +0500

    if else in react

---
+++ class6/src/crud.js
@@ -0,0 +1,15 @@
+function Crud(){
+    return(
+        <div>
+
+            {
+                false ?
+                <p>this is tru stat...</p>
+                :
+                <p>this is false stat...</p>
+            }
+
+        </div>
+    )
+}
+export default Crud;
