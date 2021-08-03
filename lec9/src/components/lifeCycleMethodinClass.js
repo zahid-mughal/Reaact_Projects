f1e847c05e154d750a8160d95df34fdedcf4eb67
zahid-mughal <zahisohna@gmail.com>
2021-08-03 08:20:36 +0500

    class comonent create

---
+++ lec9/src/components/lifeCycleMethodinClass.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class MethodinClass extends Component {
+
+    render() {
+        return (
+            <div>text in Component</div>
+        )
+
+    }
+}
+export default MethodinClass;
