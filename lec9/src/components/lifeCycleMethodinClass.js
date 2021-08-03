3787d83c3206e0061ae742278d5461498e50dc92
zahid-mughal <zahisohna@gmail.com>
2021-08-03 08:39:24 +0500

    componentWillMount after constructor

--- lec9/src/components/lifeCycleMethodinClass.js
+++ lec9/src/components/lifeCycleMethodinClass.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react'
 
 class MethodinClass extends Component {
 
+    constructor(props){
+        super(props);
+        console.log("constructor")
+    }
+    componentWillMount(){
+        console.log("componentWillMount")
+    }
+
     render() {
+        console.log("render")
         return (
             <div>text in Component</div>
         )
