0793b27b164679c3780b9fec59e4dd9d7b59a6a3
zahid-mughal <zahisohna@gmail.com>
2021-08-03 12:28:19 +0500

    allmethods done in class

--- lec9/src/components/lifeCycleMethodinClass.js
+++ lec9/src/components/lifeCycleMethodinClass.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react'
+import ChildClass from './childClass';
+
 
 class MethodinClass extends Component {
 
-    constructor(props){
+
+    constructor(props) {
         super(props);
-        console.log("constructor")
+        console.log("parent constructor")
+        this.state = { date: new Date().toISOString() }
     }
-    componentWillMount(){
-        console.log("componentWillMount")
+    componentWillMount() {
+        console.log("parent componentWillMount")
+
+    }
+    componentDidMount() {
+        console.log("parent componentDidMount");
+        // this.setState({ date: new Date().toISOString() })
+        this.clearSetId = setInterval(() => {
+            console.log("setInterval")
+            console.log("clearSetId", this.clearSetId)
+
+            this.update_date();
+        },10000);
+        //    this.clearSetId2= setInterval(()=>{
+        //     console.log("setInterval2")
+        //     console.log("clearSetId2",this.clearSetId2)
+
+        //  this.update_date();
+        // },4000);
+
+        //  console.log("clearSetId",this.clearSetId)
+        // setTimeout(() => {
+        //     console.log("setTimeout")
+        //     clearInterval(this.clearSetId)
+        //     this.update_date()
+        // }, 3000)
+    }
+    componentDidUpdate() {
+        console.log("parent componentDidUpdate")
+    }
+    componentWillUnmount() {
+        console.log("parent componentWillUnmount")
+    }
+    componentWillReceiveProps() {
+        console.log("parent componentWillReceiveProps")
+    }
+    componentDidCatch(error) {
+        console.log("parent componentDidCatch");
+    }
+    shouldComponentUpdate() {
+        console.log("parent shouldComponentUpdate");
+        return true;
+    }
+    update_date = () => {
+        this.setState({ date: new Date().toISOString() })
     }
 
     render() {
-        console.log("render")
+        console.log("parent render");
+        const { date } = this.state
         return (
-            <div>text in Component</div>
+            <div>
+                parent render Method{date}
+                <ChildClass date={date} />
+            </div>
         )
 
     }
